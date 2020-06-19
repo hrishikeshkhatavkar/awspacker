40c220b5c9a90208b6172dc3abfccc0e0c3f6bc6
Hrishikesh Khatavkar <hrishikesh.khatavkar@corp.intelliswift.com>
2020-06-19 15:52:47 +0530

    the first script for packer

--- apache.sh
+++ apache.sh
@@ -2,10 +2,11 @@
 
 sudo yum install httpd -y
 sudo service httpd start
+sudo service httpd status
 sudo chkconfig on
  
 sudo touch /var/www/html/index.html
 
 sudo chmod 777 /var/www/html/index.html
 
-echo "<hi> Version 1 </h1> >" /var/www/html/index.html
+echo "<hi> Version 1 </h1>" /var/www/html/index.html
