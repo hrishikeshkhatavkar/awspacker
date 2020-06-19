24b156df218a95bc8b6c13e3648bf2c0a8b7db85
Hrishikesh Khatavkar <hrishikesh.khatavkar@corp.intelliswift.com>
2020-06-19 15:08:22 +0530

    the autoscaling script for packer

---
+++ apache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum install httpd -y
+sudo service httpd start
+sudo chkconfig on
+ 
+sudo touch /var/www/html/index.html
+
+sudo chmod 777 /var/www/html/index.html
+
+echo "<hi> Version 1 </h1> > /var/www/html/index.html
